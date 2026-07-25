1def4485cf32d16bec26b20aa1f4d08d0f266ab0
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-25 18:15:30 +0530

    Auto commit

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Calculator")
 num1=st.number_input("Enter your first number: ")
 num2=st.number_input("Enter your second number: ")
-operation=st.selectbox("Select opertions you want to perform: ",["Addition "],["Subtraction "],["Multiplication "],["Division "])
+operation=st.selectbox("Select opertions you want to perform: ",["Addition ","Subtraction ","Multiplication ","Division "])
 if st.button("calculate"):
     if operation=="Addition":
         st.write(num1+num2)
