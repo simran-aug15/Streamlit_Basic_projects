26eafc9970de7b2ead05b2e8a76ae14227d617a6
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-25 16:48:00 +0530

    Auto commit

--- Real_Rolling_Dice_Game.py
+++ Real_Rolling_Dice_Game.py
@@ -7,5 +7,13 @@ if st.button("Roll Dice"):
     st.success(f"you got: {final}")
 
 #Dice images
-dice_images
+dice_images={
+    1: "https://game-icons.net/1x1/delapouite/dice-six-faces-one.html",
+    2: "https://game-icons.net/tags/dice.html",
+    3: "https://www.shutterstock.com/image-vector/one-dice-number-three-on-visible-1240489420",
+    4: "https://game-icons.net/tags/dice.html",
+    5: "https://www.reddit.com/r/rpg/comments/8nt7n3/i_was_annoyed_that_there_were_no_android_dice/",
+    6: "https://game-icons.net/tags/dice.html"
+
+}
     
