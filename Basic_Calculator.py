e5b5114f58c9492bddf5c092f436e6477b64b3ef
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-25 18:18:00 +0530

    Auto commit

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -3,13 +3,14 @@ st.title("Calculator")
 num1=st.number_input("Enter your first number: ")
 num2=st.number_input("Enter your second number: ")
 operation=st.selectbox("Select opertions you want to perform: ",["Addition ","Subtraction ","Multiplication ","Division "])
+
 if st.button("calculate"):
     if operation=="Addition":
-        st.write(num1+num2)
+        st.write(num1 + num2)
     elif operation=="Subtraction":
-        st.write(num1-num2)
+        st.write(num1 - num2)
     elif operation=="Multiplication":
-        st.write(num1*num2)
+        st.write(num1 * num2)
     elif operation == "Division":
         if num2 != 0:
             result = num1 / num2
