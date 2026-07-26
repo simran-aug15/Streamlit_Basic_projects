a7e17e846ee066075aca1e734be039dde1ba0504
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-26 17:32:44 +0530

    Auto commit

--- Whiteboard_Drawing.py
+++ Whiteboard_Drawing.py
@@ -7,4 +7,10 @@ stroke_width=st.sidebar.slider("Brush Size",1,20,5)
 canvas_result=st_canvas(
     fill_color="rgba(255,255,255,0)",
     stroke_width=stroke_width
+    stroke_color=color,
+    background_color="#ffffff",
+    
+
+
+
 )
