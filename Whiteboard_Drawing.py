c80ecb5375241e8ccca0c94decfdd4e3d8c3197e
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-26 17:26:49 +0530

    Auto commit

--- Whiteboard_Drawing.py
+++ Whiteboard_Drawing.py
@@ -1,4 +1,5 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-st.title("Whiteboard Drawing App")
+st.title("🎨 Whiteboard Drawing App")
 st.sidebar.header("Tools")
+color = st.sidebar.color_picker("Pick Color","#000000")
