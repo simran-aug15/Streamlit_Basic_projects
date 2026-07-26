429a9009c7e5684023c2e83447a109818eba431e
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-26 17:27:35 +0530

    Auto commit

--- Whiteboard_Drawing.py
+++ Whiteboard_Drawing.py
@@ -3,3 +3,4 @@ from streamlit_drawable_canvas import st_canvas
 st.title("🎨 Whiteboard Drawing App")
 st.sidebar.header("Tools")
 color = st.sidebar.color_picker("Pick Color","#000000")
+stroke_width=st.sidebar.slider("Brush Size",1,20,5)
