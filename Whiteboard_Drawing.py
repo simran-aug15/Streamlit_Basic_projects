38b23ea4d1c2fcae73275e479c904a08d16a1e8a
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2026-07-26 17:33:48 +0530

    Auto commit

--- Whiteboard_Drawing.py
+++ Whiteboard_Drawing.py
@@ -4,12 +4,17 @@ st.title("🎨 Whiteboard Drawing App")
 st.sidebar.header("Tools")
 color = st.sidebar.color_picker("Pick Color","#000000")
 stroke_width=st.sidebar.slider("Brush Size",1,20,5)
-canvas_result=st_canvas(
+canvas_result=st_canvas
+(
     fill_color="rgba(255,255,255,0)",
     stroke_width=stroke_width
     stroke_color=color,
     background_color="#ffffff",
-    
+    height=400,
+    width=600,
+    drawing_mode="freedraw",
+    key="canvas"
+
 
 
 
